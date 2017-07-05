902a53be797fb7db58e0df2845450fdc30a39203
buliceli <lidongdong@buliceli.local>
2017-07-05 17:33:10 +0800

    新增加了LDDTestController

---
+++ tectCplusPlus/LDDTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  LDDTestViewController.h
+//  tectCplusPlus
+//
+//  Created by 李洞洞 on 5/7/17.
+//  Copyright © 2017年 Minte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LDDTestViewController : UIViewController
+
+@end
---
+++ tectCplusPlus/LDDTestViewController.m
@@ -0,0 +1,37 @@
+//
+//  LDDTestViewController.m
+//  tectCplusPlus
+//
+//  Created by 李洞洞 on 5/7/17.
+//  Copyright © 2017年 Minte. All rights reserved.
+//
+
+#import "LDDTestViewController.h"
+
+@interface LDDTestViewController ()
+
+@end
+
+@implementation LDDTestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
