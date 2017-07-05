1d67ab6386c824e23cdd375091c6ea658884ed51
WangXinKe <13716791941@163.com>
2017-07-05 16:25:34 +0800

    New creat MrRightViewController

---
+++ tectCplusPlus/tectCplusPlus/MrRightViewController.h
@@ -0,0 +1,13 @@
+//
+//  MrRightViewController.h
+//  tectCplusPlus
+//
+//  Created by 王新克 on 2017/7/5.
+//  Copyright © 2017年 Minte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MrRightViewController : UIViewController
+
+@end
---
+++ tectCplusPlus/tectCplusPlus/MrRightViewController.m
@@ -0,0 +1,37 @@
+//
+//  MrRightViewController.m
+//  tectCplusPlus
+//
+//  Created by 王新克 on 2017/7/5.
+//  Copyright © 2017年 Minte. All rights reserved.
+//
+
+#import "MrRightViewController.h"
+
+@interface MrRightViewController ()
+
+@end
+
+@implementation MrRightViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
